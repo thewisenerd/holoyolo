9e4f68b0bce9580e746a2e9a5bdcc81449fb7ed0
Vineeth Raj <contact.twn@openmailbox.org>
2015-05-09 12:12:24 +0530

    fix styles, class mdi, add thx

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -1,6 +1,8 @@
 /* just to check if you have adblock installed
 	 we shall not have ads in our website.
 	 ...as long as I can help it.
+
+	 thx Adi (http://security.stackexchange.com/users/16228/adi) : http://security.stackexchange.com/a/36718
 */
 
 var amIHere = document.createElement("div");
