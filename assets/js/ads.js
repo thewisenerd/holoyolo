29ac2037b703328b5f8a71110a3098dee0db09f5
Vineeth Raj <contact.twn@openmailbox.org>
2015-05-09 12:01:45 +0530

    add untracked file

---
+++ assets/js/ads.js
@@ -0,0 +1,7 @@
+/* just to check if you have adblock installed
+	 we shall not have ads in our website.
+	 ...as long as I can help it.
+*/
+
+var amIHere = document.createElement("div");
+amIHere.setAttribute("id", "amIHere");
